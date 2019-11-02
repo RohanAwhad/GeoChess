7104c41850db8d3f8d64739c9190e8ef363f30e9
RohanAwhad <rohanavad007@gmail.com>
2019-11-02 18:43:02 +0000

    2:47:54. Network base created still error need to be resolved

--- generate_training_set.py
+++ generate_training_set.py
@@ -3,22 +3,31 @@
 import os
 import chess.pgn
 from state import State
+import numpy as np
+
 
 def get_dataset(num_samples=None):
     #pgn files in data/pgns folder
     X = []
     Y = []
     gn = 0
+    values = {'1/2-1/2':0, '0-1':-1, '1-0':1}
     for fn in os.listdir('data/pgns'):
         pgn = open(os.path.join("data/pgns", fn))
         while 1:
-            game = chess.pgn.read_game(pgn)
-            value = {'1/2-1/2':0, '0-1':-1, '1-0':1}[game.headers['Result']]
+            try:
+                game = chess.pgn.read_game(pgn)
+            except Exception as e:
+                break
+            res = game.headers['Result']
+            if res not in values:
+                continue
+            value = values[res]
             board = game.board()
             for i, move in enumerate(game.mainline_moves()):
                 board.push(move)
                 # TODO: extract the board moves
-                ser = State(board).serialize()[:, :, 0]
+                ser = State(board).serialize()
                 X.append(ser)
                 Y.append(value)
             print(f'Parsing game {gn}, got {len(X)} examples')
@@ -26,8 +35,11 @@ def get_dataset(num_samples=None):
             if num_samples is not None and len(X) > num_samples:
                 return X, Y
 
+    X = np.array(X)
+    Y = np.array(Y)
     return X, Y
 
 
 if __name__ == "__main__":
-    X, Y = get_dataset(1000)
+    X, Y = get_dataset(1e5)
+    np.savez("processed/dataset_100k.npz", X, Y)
